fcc9075f10675a99c9ee15b3637fd15f2e52c31a
genjutsu <andresd.ad5@gmail.com>
2022-10-12 17:32:24 -0300

    change in the folder file user.service.js

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,37 +1,35 @@
-module.exports = (body)=>{
-
+const Users = require('../models/User')
+const tokenGenerate = require('../generateToken/generateToken')
+module.exports = async (req,res)=>{
+    const {email} = req.body;
+    let token = "";
+    try {
+        const response = await Users.find({
+            email
+        })
+        if(response.length === 0){
+            const userData = new Users(
+                req.body
+            )
+            await userData.save()
+            token = tokenGenerate(userData)
+         return   res.status(200).json({
+                msg:"Usuario creado con exito",
+                user:userData,
+                token
+            })
+        }
+        token=tokenGenerate(response)
+            return res.status(200).json({
+                msg:"usuario encontrado",
+                token
+        })
+        
+    } catch (error) {
+      return  res.status(400).json({
+            msg: "algo salio mal"
+        })        
+    }
     
 }
   
-const {email} = req.body;
-let token = "";
-try {
-const response = await Users.find({
-    email
-})
-
-if(response.length === 0){
-    const userData = new Users(
-        req.body
-    )
-    await userData.save()
-    token = tokenGenerate(userData)
-    res.status(200).json({
-        msg:"Usuario creado con exito",
-        user:userData,
-        token
-    })
-}
-token=tokenGenerate(response)
-res.status(200).json({
-    msg:"usuario encontrado",
-    token
-})
-} catch (error) {
-console.log(error)
-res.status(400).json({
-    msg: "algo salio mal"
-})
-
-}
-}
