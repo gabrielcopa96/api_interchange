77f4d9fdec7610b2d661ebed1a9779f5cdfa3edd
genjutsu <andresd.ad5@gmail.com>
2022-10-12 17:39:48 -0300

    change in the folder services

--- app/services/user.service.js
+++ app/services/user.service.js
@@ -1,6 +1,12 @@
 const Users = require('../models/User')
 const tokenGenerate = require('../generateToken/generateToken')
 module.exports = async (req,res)=>{
+  /* The above code is a function that is being exported from a file called auth.js. The function is
+  called signup and it is a post request. The function is using the mongoose library to connect to a
+  mongoDB database. The function is using the mongoose library to create a new user in the database.
+  The function is also using the mongoose library to find a user in the database. The function is
+  using the jsonwebtoken library to create a token. The function is using the bcrypt library to hash
+  a password. The function is using the express library to */
     const {email} = req.body;
     let token = "";
     try {
